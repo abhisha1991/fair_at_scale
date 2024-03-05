735528e5e9280fffa1ce3010fdea1984d9b943ce
unknown <aus2101@columbia.edu>
2024-03-04 21:47:20 -0800

    commenting out digg

--- IMINFECTOR/preprocessing.py
+++ IMINFECTOR/preprocessing.py
@@ -1,6 +1,6 @@
 import os
 from weibo_preprocessing import weibo_preprocessing
-from digg_preprocessing import digg_preprocessing
+# from digg_preprocessing import digg_preprocessing
 
 
 """
@@ -17,5 +17,5 @@ if __name__ == '__main__':
         
     ans = weibo_preprocessing(os.path.join("Weibo","Init_Data"))
     print(ans)
-    ans = digg_preprocessing(os.path.join("..","Digg","Init_Data"))
-    print(ans)
+    # ans = digg_preprocessing(os.path.join("..","Digg","Init_Data"))
+    # print(ans)
